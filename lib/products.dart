7406335e9842eb57b26e8fda050bf8798e0f2d89
AGembeeded <adriengenic@gmail.com>
2019-06-22 02:27:01 +0200

    Modified build in products.dart moved if statement and building the list to a method

--- lib/products.dart
+++ lib/products.dart
@@ -2,32 +2,37 @@ import 'package:flutter/material.dart';
 
 class Products extends StatelessWidget {
   final List<String> products; //final tells that products will not be changed
-    
+
   Products(this.products);
 
   Widget _buildProductItem(BuildContext context, int index) {
     return Card(
-              child: Column(
-                children: <Widget>[
-                  Image.asset('assets/food.jpg'),
-                  Text(products[index])
-                ],
-              ),
-            );
+      child: Column(
+        children: <Widget>[
+          Image.asset('assets/food.jpg'),
+          Text(products[index])
+        ],
+      ),
+    );
+  }
+
+  // method
+  Widget _buildProductList() {
+    Widget productCard;
+    // If there are some products, redefine the productCard
+    if (products.length > 0) {
+      productCard = ListView.builder(
+        itemBuilder: _buildProductItem,
+        itemCount: products.length,
+      );
+    } else {
+      productCard = Center(child: Text('No products found, please add some'));
+    }
+    return productCard;
   }
 
   @override
   Widget build(BuildContext context) {
-    // Define a variable that is going to be our widget
-    Widget productCard = Center(child: Text('No products found, please add some'));
-
-    // If there are some products, redefine the productCard
-    if (products.length > 0) {
-      productCard = ListView.builder( // this is an inline if statement, this is called ternary expression
-      itemBuilder: _buildProductItem,
-      itemCount: products.length,
-    );
-    }
-    return productCard; 
+    return _buildProductList();
   }
 }
